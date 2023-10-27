15c9ac1806e448d22612f197158824a44f072735
jinmin.kim <jinmin.kim@fiscalnote.com>
2023-10-27 13:08:05 +0900

    feat: Added registration, prevented duplicates, hashed passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { error } from 'console';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    console.log({ dto });
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials token');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
